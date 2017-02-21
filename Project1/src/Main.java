ad29b7f290dd7608d24fb4dcf1a6eb2f96345e52
Derrick Heinemann <itderrickh@gmail.com>
2017-02-21 10:44:41 -0600

    Updating the program to write out the answer files
    Updating the program to write out the answer files

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Tokenizer t = Tokenizer.newTokenizer();
-        FileReader fr = FileReader.newFileReader("prog3.txt", t);
+        FileReader fr1 = FileReader.newFileReader("prog1.txt", t);
+        t = Tokenizer.newTokenizer();
+        FileReader fr2 = FileReader.newFileReader("prog2.txt", t);
+        t = Tokenizer.newTokenizer();
+        FileReader fr3 = FileReader.newFileReader("prog3.txt", t);
 
         try {
-            String result = fr.getTokens();
-            System.out.println(result);
+            String result1 = fr1.getTokens();
+            FileWriter.writeToFile("prog1.answer", result1);
+
+            String result2 = fr2.getTokens();
+            FileWriter.writeToFile("prog2.answer", result2);
+
+            String result3 = fr3.getTokens();
+            FileWriter.writeToFile("prog3.answer", result3);
         } catch (Exception ex) {
             System.out.println("There was a problem scanning: " + t.getString());
         }
