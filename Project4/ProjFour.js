c7ead59b84501e643c2e68429bb4d2f161017b1d
Derrick Heinemann <heined50@rlad.housing.uwosh.edu>
2017-03-30 10:48:40 -0500

    Changes. Krohn is killing me.

--- Project4/ProjFour.js
+++ Project4/ProjFour.js
@@ -13,6 +13,11 @@ Helpers
     dot = '.';
     lb = '[';
     rb = ']';
+    eq = '=';
+    exclam = '!';
+    greater = '>';
+    less = '<';
+    col = ':';
 
 Tokens
     blank = (sp | enter | lf| tab)+;
@@ -46,23 +51,28 @@ Tokens
     lcurly = '{';
     rbrace = rb;
     lbrace = lb;
-    gt = '>';
-    lt = '<';
+    gt = greater;
+    lt = less;
     plus = '+';
     minus = '-';
     times = '*';
     divide = '/';
-    colon = ':';
+    colon = col;
     semicolon = ';';
-    equals = '=';
-    exc = '!';
+    equals = eq;
+    exc = exclam;
     period = dot;
     comma = ',';
     dblquote = '"';
     subset = lb digit+ rb;
     id = letter (letter | number | underscore)*;
     realnum = digit+ dot digit+;
-    intnum = digit+;
+    intnum = digit digit*;
+    doubleequals = eq eq;
+    notequals = exclam eq;
+    gtequals = greater eq;
+    ltequals = less eq;
+    assign = col eq;
     anychar = [35..255];
 
 Ignored Tokens
@@ -70,61 +80,28 @@ Ignored Tokens
 
 Productions
     prog = begin classmethodstmts end;
-    type = {int} int |
-           {real} real |
-           {string} string |
-           {bool} bool |
-           {void} void |
-           {id} id;
-    multop = {times} times |
-             {divide} divide;
-    addop = {plus} plus |
-            {minus} minus;
-    doubleequals = equals [dbleql]:equals;
-    notequals = exc equals;
-    gtequals = gt equals;
-    ltequals = lt equals;
-    assign = colon equals;
-    cond = {dbleql} doubleequals |
-           {neql} notequals |
-           {gteql} gtequals |
-           {lteql} ltequals |
-           {gt} gt |
-           {lt} lt;
-    boolean = {true} true | {false} false | {cond} expr cond [right]:expr;
-    factor = {ffirst} lparen expr rparen |
-            {fsecond} minus factor |
-            {fthird} intnum |
-            {ffourth} realnum |
-            {ffifth} id subset? |
-            {fseventh} id lparen varlisttwo rparen |
-            {feighth} id subset? period [nextid]:id lparen varlisttwo rparen;
-
-    term = {tfirst} term multop factor |
-           {tsecond} factor;
-    expr = {efirst} expr addop term |
-           {esecond} term;
-    varlisttwotail = comma expr;
-    varlisttwo = {vlt} expr varlisttwotail* |
-                 {blank} ;
-    varlisttail = {tailemp} comma id colon type |
-                  {largetail} comma id colon type lbrace intnum rbrace ;
-    varlist = id colon type subset? varlisttail* |
-              {bl} ;
-    idtail = comma id;
+    classmethodstmts = {cmssfirst} classmethodstmts classmethodstmt |
+                       {cmsssecond} ;
+    classmethodstmt = {cmsfirst} clas id lcurly methodstmtseqs rcurly |
+                      {cmssecond} type id lparen varlist rparen lcurly stmtseq rcurly |
+                      {cmsthird} id idtail* colon type semicolon;
+    methodstmtseqs = {msssfirst} methodstmtseqs methodstmtseq |
+                     {mssssecond} ;
+    methodstmtseq = {mssfirst} type id lparen varlist rparen lcurly stmtseq rcurly |
+                    {msssecond} id idtail* colon type semicolon;
     stmtexprtail = {ipp} id plus [dblplus]:plus |
                    {imm} id minus [dblminus]:minus |
                    {exp} id assign expr;
     stmtdotidtail = period id lparen varlisttwo rparen;
-    breakpart = break semicolon;
-    morecases = case lparen intnum rparen colon stmtseq breakpart?;
+    stmtseq = {ssfirst} stmt stmtseq |
+              {sssecond} ;
     stmt = {sfirst} id subset? assign expr semicolon |
-           {ssecond} id subset? assign dblquote anychar+ [enddbl]:dblquote semicolon |
+           {ssecond} id subset? assign dblquote anychars [enddbl]:dblquote semicolon |
            {sthird} id idtail* colon type subset? semicolon |
            {sfourth} if lparen boolean rparen then lcurly stmtseq rcurly |
-           {sfifth} if lparen boolean rparen then [lcone]:lcurly [sone]:stmtseq [rcone]:rcurly else [lctwo]:lcurly [stwo]:stmtseq [rctwo]:rcurly |
+           {sfifth} if lparen boolean rparen then lcurly stmtseq rcurly else [lctwo]:lcurly [stwo]:stmtseq [rctwo]:rcurly |
            {ssixth} while lparen boolean rparen lcurly stmtseq rcurly |
-           {sseventh} for lparen type? id assign expr [sone]:semicolon boolean [stwo]:semicolon stmtexprtail lcurly stmtseq rcurly |
+           {sseventh} for lparen type? id assign expr semicolon boolean [stwo]:semicolon stmtexprtail rparen lcurly stmtseq rcurly |
            {seighth} id subset? assign get lparen rparen semicolon |
            {sninth} put lparen id subset? rparen semicolon |
            {stenth} id subset? plus [dblplus]:plus semicolon |
@@ -134,16 +111,45 @@ Productions
            {sfourteen} id subset? period [idtwo]:id lparen varlisttwo rparen stmtdotidtail* semicolon |
            {sfifteen} return expr semicolon |
            {ssixteen} id subset? assign boolean semicolon |
-           {sseventeen} switch lparen expr rparen [lcone]:lcurly case [lctwo]:lcurly intnum [rcone]:rcurly [colone]:colon [sone]:stmtseq breakpart? morecases* default [coltwo]:colon [stwo]:stmtseq [rctwo]:rcurly;
-    stmtseq = {ssfirst} stmt stmtseq |
-              {sssecond} ;
-    methodstmtseq = {mssfirst} type id lparen varlist rparen lcurly stmtseq rcurly |
-                    {msssecond} id idtail* colon type semicolon;
-
-    methodstmtseqs = {msssfirst} methodstmtseqs methodstmtseq |
-                     {mssssecond} ;
-    classmethodstmt = {cmsfirst} clas id lcurly methodstmtseqs rcurly |
-                      {cmssecond} type id lparen varlist rparen lcurly stmtseq rcurly |
-                      {cmsthird} id idtail* colon type semicolon;
-    classmethodstmts = {cmssfirst} classmethodstmts classmethodstmt |
-                       {cmsssecond} ;
+           {sseventeen} switch lparen expr rparen lcurly case [lctwo]:lparen intnum [rcone]:rparen colon stmtseq breakpart? morecases* default [coltwo]:colon [stwo]:stmtseq [rctwo]:rcurly;
+    varlisttail = {tailemp} comma id colon type |
+                  {largetail} comma id colon type subset;
+    varlist = id colon type subset? varlisttail* |
+              {bl} ;
+    varlisttwotail = comma expr;
+    varlisttwo = {vlt} expr varlisttwotail* |
+                 {blank} ;
+    idtail = comma id;
+    breakpart = break semicolon;
+    morecases = case lparen intnum rparen colon stmtseq breakpart?;
+    type = {int} int |
+           {real} real |
+           {string} string |
+           {bool} bool |
+           {void} void |
+           {id} id;
+    multop = {mult} times |
+             {div} divide;
+    addop = {plus} plus |
+            {minus} minus;
+    cond = {dbleql} doubleequals |
+           {neql} notequals |
+           {gteql} gtequals |
+           {lteql} ltequals |
+           {gt} gt |
+           {lt} lt;
+    boolean = {true} true |
+              {false} false |
+              {cond} expr cond [right]:expr;
+    factor = {ffirst} lparen expr rparen |
+            {fsecond} minus factor |
+            {fthird} intnum |
+            {ffourth} realnum |
+            {fsixth} id subset? |
+            {fseventh} id lparen varlisttwo rparen |
+            {feighth} id subset? period [nextid]:id lparen varlisttwo rparen;
+    term = {tfirst} term multop factor |
+           {tsecond} factor;
+    expr = {efirst} expr addop term |
+           {esecond} term;
+    anychars = anychar+;
